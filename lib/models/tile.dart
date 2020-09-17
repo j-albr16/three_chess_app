f98700efdb1b1e1054cc72f0351af3acbaae2062
FyndetNeo <65955494+FyndetNeo@users.noreply.github.com>
2020-09-17 19:12:39 +0200

    getMiddle

--- lib/models/tile.dart
+++ lib/models/tile.dart
@@ -9,7 +9,13 @@ class Tile {
   final bool isWhite;
   final String id;
   final Directions directions;
-
+  Point get middle{
+    double x1 = points[0].x;
+    double x2 = points[2].x;
+    double y1 = points[0].y;
+    double y2 = points[2].y;
+    return Point(x1 + 1/2 * (x1-x2).abs(), y1 + 1/2 + (y1-y2).abs());
+}
 
   Tile({@required this.points, @required this.isWhite, @required this.id, @required this.directions});
 

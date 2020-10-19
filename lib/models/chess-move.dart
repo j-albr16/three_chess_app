91bd381b93a2519b46ed5f07da534e09b57048fc
j-albr16 <j-albr16@uni-muenster.com>
2020-10-19 18:57:24 +0200

    models

--- lib/models/chess-move.dart
+++ lib/models/chess-move.dart
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import './piece.dart';
 
 import './enums.dart';
@@ -15,16 +14,3 @@ class ChessMove{
 
    ChessMove({this.initialTile, this.check, this.checkMate, this.nextTile, this.piece});
 }
-=======
-import '../models/piece.dart';
-
-enum MoveType { QueenSideRochade, KingSideRochade, Take }
-
-class ChessMove {
-  String initialTile;
-  String nextTile;
-  Piece piece;
-  bool check;
-  bool checkMate;
-}
->>>>>>> ee3e7f190547f99ab0ff90aed1cd12c85d456f19
--- lib/models/game.dart
+++ lib/models/game.dart
@@ -3,17 +3,17 @@ import 'package:three_chess/models/enums.dart';
 
 import './player.dart';
 import './chess-move.dart';
+import './piece.dart';
 
 class Game {
   List<Player> player;
   int currentPlayer;
-<<<<<<< HEAD
   List<Map<PlayerColor, ChessMove>> chessMoves;
+  List<Piece> startingBoard;
+  
+  get currentBoard {
+    //implement calc current board with starting board and made chess Moves
+  }   
 
   Game({this.player, this.currentPlayer, this.chessMoves});
-=======
-  //List<ChessMove> chessMove;
-
-  //Game({this.player, this.currentPlayer, this.chessMove});
->>>>>>> ee3e7f190547f99ab0ff90aed1cd12c85d456f19
 }

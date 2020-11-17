0db1fec3cee34c65e71bc02534d9456d0386aadb
FyndetNeo <65955494+FyndetNeo@users.noreply.github.com>
2020-11-17 21:13:52 +0100

    missed -1 in update game in three chess board widget

--- lib/helpers/user_acc.dart
+++ lib/helpers/user_acc.dart
@@ -1,2 +1,4 @@
-const constUserId = '5f997e32b5b10b72f88f33a1';
-const constToken = '97nz4l9kq83rz907tyff6ara8';
+const constUserId = '5fa2acde10f740ca2bc1265f';
+const constToken = '079f9zqnodyq2iw43r2nl8x82';
+
+const String mySecretString = "LOOK MUM IM SO CREATIVE LOL LMAO!11";

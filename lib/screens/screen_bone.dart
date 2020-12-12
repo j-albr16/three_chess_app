ac2b9193d8285c4f580a5fe1f8a834735016633b
j-albr16 <j-albr16@uni-muenster.com>
2020-12-12 13:37:44 +0100

    ScreenBoner

--- lib/screens/screen_bone.dart
+++ lib/screens/screen_bone.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 
 import '../providers/popup_provider.dart';
+import 'package:provider/provider.dart';
 
-class ScreenBone extends StatefulWidget {
+
+abstract class ScreenBone extends StatefulWidget {
   ScreenBone({Key key}) : super(key: key);
 
   @override
@@ -10,9 +12,21 @@ class ScreenBone extends StatefulWidget {
 }
 
 class _ScreenBoneState extends State<ScreenBone> {
+
+@override
+  void didChangeDependencies() {
+   super.didChangeDependencies();
+    bool hasPopup = Provider.of<PopupProvider>(context).hasPopup;
+    WidgetsBinding.instance.addPostFrameCallback((t) {
+      if (hasPopup) {
+        Provider.of<PopupProvider>(context, listen: false)
+            .displayPopup(context);
+        hasPopup = false;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-);
   }
 }

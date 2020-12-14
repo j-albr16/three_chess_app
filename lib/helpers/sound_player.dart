d5bcfc8ffea506d8e13fdfdca1f5cc99e545d5af
j-albr16 <j-albr16@uni-muenster.com>
2020-12-14 14:50:57 +0100

    sounds

--- lib/helpers/sound_player.dart
+++ lib/helpers/sound_player.dart
@@ -1,25 +1,20 @@
+import 'package:audioplayers/audio_cache.dart';
 import 'package:audioplayers/audioplayers.dart';
-enum Sound {
-  Capture, LowTime, Move, SocialNotify
-}
+
+enum Sound { Capture, LowTime, Move, SocialNotify }
 
 const Map<Sound, String> soundLinks = {
-  Sound.Capture : '../../assets/sound/favs/Capture.mp3',
-  Sound.LowTime :  '../../assets/sound/favs/LowTime.mp3',
-  Sound.Move :  '../../assets/sound/favs/Move.mp3',
-  Sound.SocialNotify :  '../../assets/sound/favs/SocialNotify.mp3',
+  Sound.Capture: 'sound/favs/Capture.mp3',
+  Sound.LowTime: 'sound/favs/LowTime.mp3',
+  Sound.Move: 'sound/favs/Move.mp3',
+  Sound.SocialNotify: 'sound/favs/SocialNotify.mp3',
 };
 
 class Sounds {
-AudioPlayer _audioPlayer = AudioPlayer();
+  static final _audioPlayer = AudioCache();
 
-
-Future<void> playSound(Sound sound) async {
+  static Future<void> playSound(Sound sound) async {
     // audio session needs to opened AND cosed
-    int result = await _audioPlayer.play(soundLinks[sound], isLocal: true);
-    if(result == 1){
-      // succes
-      print('Audio was Player');
-    }
-}
+    await _audioPlayer.play(soundLinks[sound], mode: PlayerMode.LOW_LATENCY);
+  }
 }

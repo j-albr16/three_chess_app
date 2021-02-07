eb724a96ab3d0dfb3e467484a6c467befe4dac22
FyndetNeo <65955494+FyndetNeo@users.noreply.github.com>
2021-02-07 16:38:40 +0100

    LocalProvider

--- lib/providers/local_provider.dart
+++ lib/providers/local_provider.dart
@@ -5,11 +5,33 @@ import '../models/local_game.dart';
 
 class LocalProvider with ChangeNotifier {
   List<LocalGame> localGames = [];
+  List<LocalGame> finishedLocalGames = [];
   List<LocalGame> analyzeGames = [];
 
   LocalProvider();
 
   update(){
     // TODO Not implemented
+  }
+
+  fetchCurrentGamesFromDisk(){
+
+  }
+
+  fetchFinishedGamesFromDisk(){
+
+  }
+
+  //Saves a LocalGame or FinishedLocalGame to the disk
+  saveToDisk(LocalGame game){
+
+  }
+
+  //Compares Current and Finished Games with the server
+  //to synchronise both to the newest version of each Game
+  //(That is why FinishedGames must be managed to, as they clearly indicate
+  // what games are played to end)
+  syncDiskToServer(){
+
   }
 }
